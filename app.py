a7409ef07f6df5c335b5fad5970bb6796fa25448
Jason Jon Carreos <jasondev06@gmail.com>
2025-12-05 15:57:24 +1100

    Basic styling

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from dash import Dash, html, dcc
+from dash import Dash, dcc, html, Input, Output, callback
 
 import pandas as pd
 import plotly.express as px
 
 
 DATA_PATH = "data/combined_sales_data.csv"
-
-# Show data at least 30 days before and after the date in question to better visualise
-# if there was a change in sales after the price increase
-BASIS_DATE = pd.Timestamp("2021-01-15")
-START_DATE = BASIS_DATE - pd.Timedelta(days=30)
-END_DATE = BASIS_DATE + pd.Timedelta(days=30)
 
 app = Dash()
 
@@ -19,32 +13,42 @@ df = pd.read_csv(DATA_PATH, parse_dates=["date"])
 
 df.sort_values(by="date", inplace=True)
 
-grouped_df = df.groupby("date")["sales"].sum().reset_index()
-filtered_df = grouped_df[(grouped_df["date"] >=
-                          START_DATE) & (grouped_df["date"] <= END_DATE)]
+REGIONS = list(map(lambda x: x.capitalize(),
+               df["region"].unique().tolist() + ['all']))
 
-# Prepare figure
-fig = px.line(filtered_df, template="seaborn", x="date", y="sales", labels={
-              "date": "Date", "sales": "Sales ($)"}, markers=True)
 
-fig.add_annotation(
-    x=BASIS_DATE,
-    y=filtered_df[filtered_df["date"] == BASIS_DATE]["sales"].values[0],
-    text=BASIS_DATE.strftime("%b %d %Y"),
-    showarrow=False,
-    xanchor="right",
-    yanchor="bottom"
+@callback(
+    Output("pink-morsel-graph", "figure"),
+    Input("radio", "value")
 )
+def update_figure_by_region(selected_region):
+    filtered_df = df.copy()
+
+    if selected_region != REGIONS[-1]:
+        filtered_df = df[df["region"] == selected_region.lower()]
+
+    # Prepare figure
+    fig = px.line(filtered_df, template="seaborn", x="date", y="sales", color="region", labels={
+        "date": "Date", "sales": "Sales ($)", "region": "Region"}, markers=True)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
 
 app.layout = html.Div(children=[
     html.H1(children="Soul Foods's Pink Morsel Sales Analysis"),
     html.Div(children="""
         Were sales higher before or after the Pink Morsel price increase on the 15th of January, 2021?
         """),
-    dcc.Graph(
-        id="pink-morsel-graph",
-        figure=fig
-    )
+
+    dcc.Graph(id="pink-morsel-graph"),
+
+    html.Label('Region:'),
+    dcc.RadioItems(REGIONS, value=REGIONS[-1], id="radio", labelStyle={'display': 'inline-block', 'margin-right': '20px', 'fontSize': '16px'},
+                   inputStyle={'margin-right': '8px',
+                               'width': '16px', 'height': '16px'},
+                   style={'padding': '8px'})
 ], style={"width": "80%", "margin": "0 auto", "font-family": "Helventica Neue, sans-serif"})
 
 if __name__ == "__main__":
